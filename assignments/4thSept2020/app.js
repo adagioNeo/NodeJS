c75dc4fb66adf3364100a6b2821dc0aea22b78f1
adagioneo <vamsi@netdox.org>
2021-09-10 19:03:15 +0530

    done with assignment on 5th

--- assignments/4thSept2020/app.js
+++ assignments/4thSept2020/app.js
@@ -5,7 +5,25 @@ const express = require('express'),
 const app = new express,
     employees = [
         {
-            id:"sample",name:"Sample",number:"+91999999999"
+            id:"r12UK99",name:"Hari Haran",number:"+91999999999"
+        },
+        {
+            id:"shmk2UK99",name:"Virendra Sharma",number:"+919977899976"
+        },
+        {
+            id:"prdy2UK99",name:"Poorna Reddy",number:"+919667869970"
+        },
+        {
+            id:"hnSk2UK99",name:"Heena Sagar",number:"+919973976976"
+        },
+        {
+            id:"mhsg2UK99",name:"Mahender Sigh",number:"+919876899977"
+        },
+        {
+            id:"pjkl2UK99",name:"Pooja Kohli",number:"+919347899954"
+        },
+        {
+            id:"mnrt2UK99",name:"Mani Ratan",number:"+919978799973"
         }
     ],
     queryList = [];
@@ -30,7 +48,6 @@ app.get("/contact",function(req,res){
     res.sendFile(path.join(__dirname,"/contact.html"));
 });
 app.get("/contactFormSubmission",function(req,res){
-    console.log(req.query)
     res.render(path.join(__dirname,"/contactFormSubmission.html"),{token: req.query.query});
 })
 app.get("/viewLogin",function(req,res){
