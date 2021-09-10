5e7bd262a53c4c655e385e5e4bd5c598560c3062
adagioneo <vamsi@netdox.org>
2021-09-10 17:47:17 +0530

    added first assignment of 4th

---
+++ assignments/4thSept/app.js
@@ -0,0 +1,36 @@
+const express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    PORT1=5500;
+const app = new express,
+    employees = [
+        {
+            id:"sample",name:"Sample",number:"+91999999999"
+        }
+    ],
+    queryList = [];
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname,"/home.html"));
+});
+app.get("/employees",function(req,res){
+    let body = ``;
+    employees.forEach((employee)=>{
+        body+= `
+        <tr class="table-primary">
+            <th scope="row">${employee.id}</th>
+            <td>${employee.name}</td>
+            <td>${employee.number}</td>
+        </tr>`
+    })
+    res.render(path.join(__dirname,"/employees.html"),{token: body.toString()});
+});
+app.get("/contact",function(req,res){
+    res.sendFile(path.join(__dirname,"/contact.html"));
+});
+app.get("/contactFormSubmission",function(req,res){
+    console.log(req.query)
+    res.render(path.join(__dirname,"/contactFormSubmission.html"),{token: req.query.query});
+})
+app.listen(PORT1);
