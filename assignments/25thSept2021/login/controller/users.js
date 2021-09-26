c797ba8188383c207b2f40d6c8ca9793cd532645
adagioneo <vamsi@netdox.org>
2021-09-26 10:24:24 +0530

    updated main

--- assignments/25thSept2021/login/controller/users.js
+++ assignments/25thSept2021/login/controller/users.js
@@ -1,57 +1,73 @@
 const mongoose = require('mongoose');
 const Users = require('../models/users');
 const LoginDetails = require("../models/login");
-exports.addUser = function(req,res){
-    const {username, password, mail} = req.body;
+const { json } = require('express');
+exports.addUser = function (req, res) {
+    const { username, password, mail } = req.body;
     const user = new Users({
         username, password, mail
     });
-    user.save(function(err){
-        if(err){
-            res.json({status: 400, data_err: "Db: Insert error"});
+    user.save(function (err) {
+        if (err) {
+            res.json({ status: 400, data_err: "Db: Insert error" });
         }
-        res.json({status: 200, data: "Added User"});
+        res.json({ status: 200, data: "Added User" });
     })
 };
-exports.updateUser = function(req,res){
-    const {username, new_password, new_mail} = req.body;
-    Users.findOneAndUpdate(function(err){
-        if(err){
-            res.json({status: 400, data_err: "Db: Update error NOTE: Username cannot be edited"});
+exports.updateUser = function (req, res) {
+    const { username, new_password, new_mail } = req.body;
+    Users.findOneAndUpdate(function (err) {
+        if (err) {
+            res.json({ status: 400, data_err: "Db: Update error NOTE: Username cannot be edited" });
         }
-        res.json({status: 200, data: "Update User"});
+        res.json({ status: 200, data: "Update User" });
     })
 };
-exports.fetchUser = function(req,res){
+exports.fetchUser = function (req, res) {
     let reqObj = req.body;
-    Users.findById(reqObj.id,function(reqObj,res){
+    Users.findById(reqObj.id, function (reqObj, res) {
 
     });
 };
-exports.removeUser = function(req,res){
-    const {username, password, mail} = req.body;
+exports.fetchAllUsers = function (req, res) {
+    let reqObj = req.body;
+    Users.find(function (err, res) {
+        if (err) {
+            json.res({
+                status: 400,
+                error: "MongoDb failed to fetch data"
+            })
+        }
+        json.res({
+            status: 200,
+            data: res
+        })
+    });
+}
+exports.removeUser = function (req, res) {
+    const { username, password, mail } = req.body;
     const user = Users.deleteOne({
         username, password, mail
     });
-    user.save(function(err){
-        if(err){
-            res.json({status: 400, data_err: "Db: Delete error"});
+    user.save(function (err) {
+        if (err) {
+            res.json({ status: 400, data_err: "Db: Delete error" });
         }
-        res.json({status: 200, data: "Delete User"});
+        res.json({ status: 200, data: "Delete User" });
     })
 }
-exports.loginUser = function(req,res){
-    const {username, password} = req.body;
-    Users.find(function(err){
+exports.loginUser = function (req, res) {
+    const { username, password } = req.body;
+    Users.findById(function (err) {
         if
     })
     const user = new LoginDetails({
         username, password
     });
-    user.save(function(err){
-        if(err){
-            res.json({status: 400, data_err: "Db: Session creation error"});
+    user.save(function (err) {
+        if (err) {
+            res.json({ status: 400, data_err: "Db: Session creation error" });
         }
-        res.json({status: 200, data: "User logged in successfully"});
+        res.json({ status: 200, data: "User logged in successfully" });
     })
 }
